fd887e76e41be88369e38657f69203b85a0ff2c9
dchiu1998 <44936461+dchiu1998@users.noreply.github.com>
2018-11-15 23:13:31 -0500

    Add files via upload

---
+++ salesapp/exceptions/exceptions/CreateUserFailedException.java
@@ -0,0 +1,10 @@
+package com.b07.exceptions;
+
+public class CreateUserFailedException extends Exception {
+
+  /**
+   * SerialID for connection FailToCreateNewUserException.
+   */
+  private static final long serialVersionUID = 1L;
+
+}
